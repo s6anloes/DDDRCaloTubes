d8ebe0840ca1b0e682e6927af7952161e7ed96f6
Andreas Loeschcke Centeno <andreas.loeschcke.centeno@cern.ch>
2023-02-22 15:37:37 +0100

    Added fibre cladding and cleaned up code

--- src/DDDRCaloTubes.cpp
+++ src/DDDRCaloTubes.cpp
@@ -18,8 +18,6 @@ static Ref_t create_detector(Detector& description,
     Material    brass       = description.material("Brass");
 
     xml_dim_t   x_dim       = x_det.dimensions();
-    double      rmin        = x_dim.rmin();
-    double      rmax        = x_dim.rmax();
     double      z_half      = x_dim.zmax();
 
     DetElement    s_detElement(det_name, det_id);
@@ -29,57 +27,92 @@ static Ref_t create_detector(Detector& description,
     sens.setType("calorimeter");
 
 
+    // Get parameters for tube construction
     xml_comp_t  x_tube      = x_det.child(_Unicode(tube));
     int         num_rows    = x_tube.number();
     int         num_cols    = x_tube.count();
 
-    xml_comp_t  x_capillary         = x_tube.child(_Unicode(capillary));
-    Material    capillary_material  = description.material(x_capillary.materialStr()); 
-    double      capillary_inner_r   = x_capillary.inner_r();
-    double      capillary_outer_r   = x_capillary.outer_r();
+    xml_comp_t  x_capillary          = x_tube.child(_Unicode(capillary));
+    Material    capillary_material   = description.material(x_capillary.materialStr()); 
+    double      capillary_outer_r    = x_capillary.outer_r();
+
+    xml_comp_t  x_scin_clad          = x_tube.child(_Unicode(scin_clad));
+    Material    scin_clad_material   = description.material(x_scin_clad.materialStr()); 
+    double      scin_clad_outer_r    = x_scin_clad.outer_r();
 
     xml_comp_t  x_scin_fibre         = x_tube.child(_Unicode(scin_fibre));
     Material    scin_fibre_material  = description.material(x_scin_fibre.materialStr()); 
-    double      scin_fibre_inner_r   = x_scin_fibre.inner_r();
     double      scin_fibre_outer_r   = x_scin_fibre.outer_r();
+
+    xml_comp_t  x_cher_clad          = x_tube.child(_Unicode(cher_clad));
+    Material    cher_clad_material   = description.material(x_cher_clad.materialStr()); 
+    double      cher_clad_outer_r    = x_cher_clad.outer_r();
 
     xml_comp_t  x_cher_fibre         = x_tube.child(_Unicode(cher_fibre));
     Material    cher_fibre_material  = description.material(x_cher_fibre.materialStr()); 
-    double      cher_fibre_inner_r   = x_cher_fibre.inner_r();
     double      cher_fibre_outer_r   = x_cher_fibre.outer_r();
 
-    Tube            capillary_solid(0.0*mm, capillary_outer_r, z_half);
-    std::string     capillary_name = "capillary";//_"+std::to_string(row)+"_"+std::to_string(col);
+
+    // Construct volumes for tubes
+    Tube        capillary_solid(0.0*mm, capillary_outer_r, z_half);
+    std::string capillary_name = "capillary";//_"+std::to_string(row)+"_"+std::to_string(col);
     // Need two volumes for scintillation and Cherenkov channels
-    Volume          scin_tube_volume(capillary_name, capillary_solid, capillary_material);
-    Volume          cher_tube_volume(capillary_name, capillary_solid, capillary_material);
+    Volume      scin_tube_volume("scin_"+capillary_name, capillary_solid, capillary_material);
+    Volume      cher_tube_volume("cher_"+capillary_name, capillary_solid, capillary_material);
     if (x_capillary.isSensitive())
     {
         scin_tube_volume.setSensitiveDetector(sens);
         cher_tube_volume.setSensitiveDetector(sens);
     }
-    scin_tube_volume.setAttributes(description, x_capillary.regionStr(), x_capillary.limitsStr(), x_capillary.visStr());
-    cher_tube_volume.setAttributes(description, x_capillary.regionStr(), x_capillary.limitsStr(), x_capillary.visStr());    
+    scin_tube_volume.setVisAttributes(description, x_capillary.visStr());
+    cher_tube_volume.setVisAttributes(description, x_capillary.visStr());    
 
-    Tube            scin_fibre_solid(0.0*mm, scin_fibre_outer_r, z_half);
-    std::string     scin_fibre_name = "scin_fibre";//_"+std::to_string(row)+"_"+std::to_string(col);
-    Volume          scin_fibre_volume(scin_fibre_name, scin_fibre_solid, scin_fibre_material);
+
+    // Scintillation cladding
+    Tube        scin_clad_solid(0.0*mm, scin_clad_outer_r, z_half);
+    std::string scin_clad_name = "scin_clad";
+    Volume      scin_clad_volume(scin_clad_name, scin_clad_solid, scin_clad_material);
+    if (x_scin_clad.isSensitive())
+    {
+        scin_clad_volume.setSensitiveDetector(sens);
+    }
+    PlacedVolume scin_clad_placed = scin_tube_volume.placeVolume(scin_clad_volume);
+    scin_clad_volume.setVisAttributes(description, x_scin_clad.visStr());
+
+    // Scintillation fibre
+    Tube        scin_fibre_solid(0.0*mm, scin_fibre_outer_r, z_half);
+    std::string scin_fibre_name = "scin_fibre";//_"+std::to_string(row)+"_"+std::to_string(col);
+    Volume      scin_fibre_volume(scin_fibre_name, scin_fibre_solid, scin_fibre_material);
     if (x_scin_fibre.isSensitive())
     {
         scin_fibre_volume.setSensitiveDetector(sens);
     }
-    PlacedVolume    scin_fibre_placed = scin_tube_volume.placeVolume(scin_fibre_volume);
-    scin_fibre_volume.setAttributes(description, x_scin_fibre.regionStr(), x_scin_fibre.limitsStr(), x_scin_fibre.visStr());
+    PlacedVolume    scin_fibre_placed = scin_clad_volume.placeVolume(scin_fibre_volume);
+    scin_fibre_volume.setVisAttributes(description, x_scin_fibre.visStr());
+    scin_fibre_placed.addPhysVolID("fibre", 1).addPhysVolID("cherenkov", 0);
 
-    Tube            cher_fibre_solid(0.0*mm, cher_fibre_outer_r, z_half);
-    std::string     cher_fibre_name = "cher_fibre";//_"+std::to_string(row)+"_"+std::to_string(col);
-    Volume          cher_fibre_volume(cher_fibre_name, cher_fibre_solid, cher_fibre_material);
+    // Cherenkov cladding
+    Tube        cher_clad_solid(0.0*mm, cher_clad_outer_r, z_half);
+    std::string cher_clad_name = "cher_clad";
+    Volume      cher_clad_volume(cher_clad_name, cher_clad_solid, cher_clad_material);
+    if (x_cher_clad.isSensitive())
+    {
+        cher_clad_volume.setSensitiveDetector(sens);
+    }
+    PlacedVolume cher_clad_placed = cher_tube_volume.placeVolume(cher_clad_volume);
+    cher_clad_volume.setVisAttributes(description, x_cher_clad.visStr());
+
+    // Chrerenkov fibre
+    Tube        cher_fibre_solid(0.0*mm, cher_fibre_outer_r, z_half);
+    std::string cher_fibre_name = "cher_fibre";//_"+std::to_string(row)+"_"+std::to_string(col);
+    Volume      cher_fibre_volume(cher_fibre_name, cher_fibre_solid, cher_fibre_material);
     if (x_cher_fibre.isSensitive())
     {
         cher_fibre_volume.setSensitiveDetector(sens);
     }
-    PlacedVolume    cher_fibre_placed = cher_tube_volume.placeVolume(cher_fibre_volume);
-    cher_fibre_volume.setAttributes(description, x_cher_fibre.regionStr(), x_cher_fibre.limitsStr(), x_cher_fibre.visStr());
+    PlacedVolume    cher_fibre_placed = cher_clad_volume.placeVolume(cher_fibre_volume);
+    cher_fibre_volume.setVisAttributes(description, x_cher_fibre.visStr());
+    cher_fibre_placed.addPhysVolID("fibre", 1).addPhysVolID("cherenkov", 1);
 
 
     int tube_id = 0;
